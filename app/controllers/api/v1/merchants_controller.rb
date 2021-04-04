8407e389e64f48e717f3b75c7a3c64d2b8bcdcde
Joseph Budina <joebud3@gmail.com>
2021-04-03 18:10:53 -0600

    Add sad paths to merchant controller and clean up test file

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,12 +1,28 @@
 class Api::V1::MerchantsController < ApplicationController
+before_action :set_page, only: [:index]
 
   def index
-    @merchants = Merchant.all
-    render json: MerchantSerializer.new(@merchants)
+    if params[:per_page]
+      @merchants = Merchant.limit(params[:per_page].to_i).offset((@page.to_i - 1) * 20)
+      render json: MerchantSerializer.new(@merchants)
+    else params[:per_page].nil?
+      @merchants = Merchant.limit(20).offset((@page.to_i - 1) * 20)
+      render json: MerchantSerializer.new(@merchants)
+    end
   end
 
   def show
     @merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(@merchant)
+  end
+
+  private
+
+  def set_page
+    if params[:page].nil? || params[:page].to_i >= 1
+      @page = params[:page] || 1
+    else
+      @page = 1
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants
+      resources :merchants, only: [:index, :show] do
+        resources :items
+      end
     end
   end
 end
