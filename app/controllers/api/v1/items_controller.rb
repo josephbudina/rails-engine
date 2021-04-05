4f5172c416540d5f540501c0dbe514f9d6a8760f
Joseph Budina <joebud3@gmail.com>
2021-04-04 20:15:21 -0600

    Add items_controller with index and show, move item merchants

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,27 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :set_page, only: [:index]
   def index
-    @merchant = Merchant.find_by(id: params[:merchant_id])
+    if params[:per_page]
+      @items = Item.limit(params[:per_page].to_i).offset((@page.to_i - 1) * 20)
+      render json: ItemSerializer.new(@items)
+    else params[:per_page].nil?
+      @items = Item.limit(20).offset((@page.to_i - 1) * 20)
+      render json: ItemSerializer.new(@items)
+    end
+  end
 
-    if @merchant
-      render json: ItemSerializer.new(@merchant.items)
+  def show
+    @item = Item.find(params[:id])
+    render json: ItemSerializer.new(@item)
+  end
+
+  private
+
+  def set_page
+    if params[:page].nil? || params[:page].to_i >= 1
+      @page = params[:page] || 1
     else
-      not_found
+      @page = 1
     end
   end
 end
