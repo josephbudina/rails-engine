95d3037db52fff86ec69a0fcf1716362d2928a46
Joseph Budina <joebud3@gmail.com>
2021-04-06 16:51:10 -0600

    Change find method from index to show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'merchants/:id/items', to: 'merchants/merchant_items#index'
       get 'items/:id/merchant', to: 'items/items_merchant#index'
-      get 'merchants/find', to: 'merchants/find#index'
+      get 'merchants/find', to: 'merchants/find#show'
       resources :merchants, only: [:index, :show] do
       end
     resources :items
