70f85c1c811f1e63108f56fa32deffbd060042d8
Joseph Budina <joebud3@gmail.com>
2021-04-06 16:27:26 -0600

    Add merchants/find path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'merchants/:id/items', to: 'merchants/merchant_items#index'
       get 'items/:id/merchant', to: 'items/items_merchant#index'
+      get 'merchants/find', to: 'merchants/find#index'
       resources :merchants, only: [:index, :show] do
       end
     resources :items
