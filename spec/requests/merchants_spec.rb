71d5f4d25a6f237219e49d50edeff173fd611f88
Joseph Budina <joebud3@gmail.com>
2021-04-06 13:16:39 -0600

    Add specs for index, show, and set page actions

--- spec/requests/merchants_spec.rb
+++ spec/requests/merchants_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API', type: :request do
-  let!(:merchants) { create_list(:merchant, 20) }
+  let!(:merchants) { create_list(:merchant, 100) }
   let(:merchant_id) { merchants.first.id }
 
   describe 'GET /merchants' do
@@ -10,7 +10,7 @@ RSpec.describe 'Merchants API', type: :request do
 
     it 'returns Merchants' do
       expect(json).not_to be_empty
-      expect(json['data'].size).to eq(20)
+      expect(json[:data].size).to eq(20)
     end
 
     it 'returns status code 200' do
@@ -18,18 +18,72 @@ RSpec.describe 'Merchants API', type: :request do
     end
   end
 
+  describe 'get merchants 1' do
+
+    before { get 'http://localhost:3000/api/v1/merchants?page=1' }
+
+    it 'Has first 20 on page 1' do
+
+      expect(json[:data].size).to eq(20)
+      expect(json[:data][0][:attributes][:name]).to eq(merchants.first.name)
+      expect(json[:data].last[:attributes][:name]).to eq(merchants[19].name)
+    end
+  end
+
+  describe 'Check page 0' do
+    before { get 'http://localhost:3000/api/v1/merchants?page=0' }
+    it 'starts on page one if number below one is given' do
+
+      expect(json[:data].size).to eq(20)
+      expect(json[:data][0][:attributes][:name]).to eq(merchants.first.name)
+      expect(json[:data].last[:attributes][:name]).to eq(merchants[19].name)
+    end
+  end
+
+  describe 'list all merchants' do
+    before { get 'http://localhost:3000/api/v1/merchants?page=1&per_page=200' }
+
+    it 'Lists all merchants if given a large page' do
+      expect(json[:data].size).to eq(100)
+    end
+  end
+
   describe 'GET api/v1/merchants/:id' do
+    
     before { get api_v1_merchant_url(merchant_id) }
 
     context 'when the record exists' do
       it 'returns the merchant' do
+
         expect(json).not_to be_empty
-        expect(json['data']['id'].to_i).to eq(merchant_id)
+        expect(json[:data][:id].to_i).to eq(merchant_id)
       end
 
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
+    end
+  end
+
+  describe 'No record exists' do
+    context 'when the record doesnt exist' do
+
+      it 'returns a 404' do
+        expect{ get api_v1_merchant_url('1010192983743354634') }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe 'Get a merchants items' do
+    before { @item_1 = merchants.first.items.create(attributes_for(:item)) }
+    before { @item_2 = merchants.first.items.create(attributes_for(:item)) }
+    before { get "http://localhost:3000/api/v1/merchants/#{merchants.first.id}/items" }
+    
+    it 'finds all items' do
+
+      expect(json[:data].size).to eq(2)
+      expect(json[:data][0][:attributes][:name]).to eq(@item_1.name)
+      expect(json[:data].last[:attributes][:name]).to eq(@item_2.name)
     end
   end
 end
