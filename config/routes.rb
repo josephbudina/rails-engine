4023d2f378c677f1ba103bdcc6f4f71995d99825
Joseph Budina <joebud3@gmail.com>
2021-04-06 13:19:33 -0600

    Add route for items merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'merchants/items', to: 'merchants/merchant_items#index'
+      get 'merchants/:id/items', to: 'merchants/merchant_items#index'
+      get 'items/:id/merchant', to: 'items/items_merchant#index'
       resources :merchants, only: [:index, :show] do
       end
     resources :items
