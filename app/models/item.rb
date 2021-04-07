6efe0f716179cb2e59153ec7b9d0e9090101b76e
Joseph Budina <joebud3@gmail.com>
2021-04-07 10:47:11 -0600

    Add max and min actions

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates :name, :description, :unit_price, presence: true
+
+  def self.find_by_name(name)
+     where("name ILIKE ? or description LIKE ?", "%#{name}%", "%#{name}%")
+  end
+
+  scope :find_by_min_price, -> (price) { where('unit_price >= ?', price.to_i).order(:name)}
+  
+  scope :find_by_max_price, -> (price) { where('unit_price <= ?', price.to_i).order(:name)}
 end
