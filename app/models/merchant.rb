6925760aa59ac682c2b067d7bd71bbf6b6bcb5d7
Joseph Budina <joebud3@gmail.com>
2021-04-07 16:09:03 -0600

    Add revenue top revenue and index actions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,21 @@ class Merchant < ApplicationRecord
   def self.find_one(name)
     order(name: :asc).find_by("name ILIKE ?", "%#{name}%")
   end
+
+  def self.top_revenue(quantity)
+    joins(:transactions)
+    .where("transactions.result = ?", :success)
+    .select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+    .group(:id)
+    .order(total_revenue: :desc)
+    .limit(quantity)
+  end
+
+  def revenue
+    transactions
+    .where("transactions.result = ?", :success)
+    .pluck("sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+    .first
+    .round(2)
+  end
 end
