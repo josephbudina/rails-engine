21ebe9a3d934d2e619828b73387987e0d4265ec7
Joseph Budina <joebud3@gmail.com>
2021-04-07 17:13:19 -0600

    Refactor index action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,16 +1,12 @@
 class Api::V1::ItemsController < ApplicationController
   before_action :set_page, only: [:index]
+  before_action :set_offset, only: [:index]
   before_action :set_merchant, only: [:create]
   before_action :set_merchant_item, only: [:show, :update, :destroy]
 
-  def index
-    if params[:per_page]
-      items = Item.limit(params[:per_page].to_i).offset((@page.to_i - 1) * 20)
+  def index 
+      items = Item.limit(@per_page).offset((@page - 1) * @per_page)
       render json: ItemSerializer.new(items)
-    elsif params[:per_page].nil?
-      items = Item.limit(20).offset((@page.to_i - 1) * 20)
-      render json: ItemSerializer.new(items)
-    end
   end
 
   def show
@@ -35,11 +31,19 @@ class Api::V1::ItemsController < ApplicationController
 
   private
 
-  def set_page
-    if params[:page].nil? || params[:page].to_i >= 1
-      @page = params[:page] || 1
+  def set_offset
+    if params[:per_page].nil? || params[:per_page].to_i < 1
+      @per_page = 20
     else
+      @per_page = params[:per_page].to_i
+    end
+  end
+
+  def set_page
+    if params[:page].nil? || params[:page].to_i < 1
       @page = 1
+    else
+      @page = params[:page].to_i
     end
   end
 
