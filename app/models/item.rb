e1f1caf93493f8c22a9b74db70fbff5a39412312
Joseph Budina <joebud3@gmail.com>
2021-04-08 13:00:51 -0600

    Add top revenue and revenue methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,22 @@ class Item < ApplicationRecord
   scope :find_by_min_price, -> (price) { where('unit_price >= ?', price.to_i).order(:name)}
   
   scope :find_by_max_price, -> (price) { where('unit_price <= ?', price.to_i).order(:name)}
+
+  def self.top_revenue(quantity)
+    joins(invoices: :transactions)
+    .where("transactions.result = ?", :success)
+    .select("items.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+    .group(:id)
+    .order(total_revenue: :desc)
+    .limit(quantity)
+  end
+
+  def revenue
+    invoices
+    .joins(:transactions)
+    .where("transactions.result = ?", :success)
+    .pluck("sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+    .first
+    .round(2)
+  end
 end
